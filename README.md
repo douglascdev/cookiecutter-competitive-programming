f97332e55ef005391898de0f10a9311d07b0f539
Douglas Carvalho <douglasc.dev@gmail.com>
2021-05-01 15:24:47 -0300

    Added usage instructions and docstrings

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Cookiecutter for Python Competitive Programming
-A cookiecutter template for competitive programming with mocked input and output to facilitate testing/TDD.
+A cookiecutter template for competitive programming with mocked input and output to facilitate testing/TDD. 
+
+The advantage of this approach is that you can easily create methods to test inputs and outputs replacing values that are entered with stdin and stdout by entering the values in a string, and then just paste your solution on the contest directly.
+
+## Usage
+Install cookiecutter:
+```
+pip install cookiecutter
+```
+Create a project using the template:
+```
+cookiecutter https://github.com/douglascdev/cookiecutter-competitive-programming
+```
+You'll be asked for the project name, which will be created at the current folder.
+
+
+After that, you can implement the tests to make sure your code will work on the file ```cp_problem_tests.py```, and your solution at ```cp_problem.py```.
+
+Further explanations are on the files.
--- {{ cookiecutter.project_name }}/cp_problem_tests.py
+++ {{ cookiecutter.project_name }}/cp_problem_tests.py
@@ -5,22 +5,39 @@ import cp_problem
 
 
 class CPProblemTests(TestCase):
-    def io_test(self, problem_input, problem_output, method=cp_problem.problem):
+    """
+    Each method starting with "test_" will run when you run this script.
+    You can copy the "test_problem" method and simply replace the variables with the input and expected output.
+    """
+    def io_test(self, problem_input, problem_output, method=cp_problem.problem) -> None:
+        """
+        Mocks the input method and the standard output and tests if the expected output is printed.
+        :param problem_input: input that will be mocked into input() calls within the function
+        :param problem_output: the output that the method is expected to print()
+        :param method: method that will be called with mocked input and output, cp_problem.problem by default
+        """
         with patch('builtins.input', return_value=problem_input), patch('sys.stdout', new=StringIO()) as fake_out:
             method()
             self.assertEqual(fake_out.getvalue(), problem_output)
 
     @staticmethod
-    def method():
+    def method() -> None:
         _ = input()
         print("1", end="")
 
-    def test_io_test(self):
+    def test_io_test(self) -> None:
+        """
+        Tests if the mock function is working as intended
+        """
         problem_input = """1"""
         problem_output = """1"""
         self.io_test(problem_input, problem_output, self.method)
 
-    def test_problem(self):
+    def test_problem(self) -> None:
+        """
+        Enter the input and expected output below. Running this file will run this test automatically.
+        Copy and paste this method to create multiple tests.
+        """
         problem_input = """"""
         problem_output = """"""
         self.io_test(problem_input, problem_output)
