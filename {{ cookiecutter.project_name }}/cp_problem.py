14e8b240d157c0314f5949e4bb3659fa6087b466
Douglas Carvalho <douglasc.dev@gmail.com>
2021-05-05 13:45:56 -0300

    Removed unnecessary code from the problem method, left only test cases input

--- {{ cookiecutter.project_name }}/cp_problem.py
+++ {{ cookiecutter.project_name }}/cp_problem.py
@@ -1,6 +1,2 @@
 def problem():
     num_test_cases = int(input())
-    for _ in range(num_test_cases):
-        space_separated_ints = [int(i) for i in input().split(" ")]
-        for i in space_separated_ints:
-            pass
